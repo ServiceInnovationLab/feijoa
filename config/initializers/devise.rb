10bedd1313401c969bb02f9c93a509b54a98ea51
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-05 11:25:02 +1200

    enable multiple devise views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,7 +236,10 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  # 
+  # Lyall: Enabled because we expect to support Users, Admins, and ELSs (or some
+  # kind of trusted message receiver)
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
