1785b5e3dc58b1ca6cb7e58379f00b0eeed29ab0
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-06-04 16:35:15 +1200

    Set up generators

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Feijoa
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.orm :active_record
+      g.test_framework :rspec, fixture: false
+      g.javascript_engine :js
+      g.factory_bot true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
