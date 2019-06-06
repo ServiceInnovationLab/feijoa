ecaf2d2dafcfc9cc4efa1d4e28200cde96f88486
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-07 11:15:52 +1200

    tidying

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class User::BaseController < ApplicationController
+  # Only Users can access user controller actions
   before_action :authenticate_user!
 
+  # Authenticated users are allowed to view User pages
+  # This removes the deny-by-default added to ApplicationController
   # rubocop:disable Rails/LexicallyScopedActionFilter
   skip_before_action :redirect_authenticated_users
   # rubocop:enable Rails/LexicallyScopedActionFilter
