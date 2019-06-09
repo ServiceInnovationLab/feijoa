6f0e8e633795303de5a2cc7a63f47b76317a21fb
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-10 11:44:21 +1200

    request tests for administrate access

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Enable Devise spec helpers for request tests (e.g. `sign_in user`)
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
---
+++ spec/request/administrate_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'use Administrate dashboards', type: :request do
+
+  context 'An Admin' do
+    let(:admin) { FactoryBot.create(:admin_user) }
+
+    it 'can view the default dashboard' do
+      sign_in admin
+      get admin_root_path
+
+      expect(response).to be_successful
+    end
+  end
+
+  context 'A User' do
+    let(:user) { FactoryBot.create(:user) }
+
+    it 'is redirected to the AdminUser login' do
+      sign_in user
+      get admin_root_path
+
+      expect(response).to be_redirect
+      expect(response.redirect_url).to eq(new_admin_user_session_url)
+    end
+  end
+
+  context 'A guest' do
+    it 'is redirected to the AdminUser login' do
+      get admin_root_path
+
+      expect(response).to be_redirect
+      expect(response.redirect_url).to eq(new_admin_user_session_url)
+    end
+  end
+end
