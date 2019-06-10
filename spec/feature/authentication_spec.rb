d96d216d00187269311e3ca9746b9fe53c2497e3
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-10 16:54:44 +1200

    remove requests status checks not available in browser driver

--- spec/feature/authentication_spec.rb
+++ spec/feature/authentication_spec.rb
@@ -11,40 +11,36 @@ RSpec.feature 'Authentication' do
     it 'can view the home page' do
       visit root_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(root_path)
     end
 
     it 'can view the user login page' do
       visit new_user_session_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(new_user_session_path)
     end
 
     it 'can view the user sign-up page' do
       visit new_user_registration_path
-      expect(page.status_code).to eq(200)
+
       expect(page.current_path).to eq(new_user_registration_path)
     end
 
     it 'can view the admin login page' do
       visit new_admin_session_path
-      expect(page.status_code).to eq(200)
+
       expect(page.current_path).to eq(new_admin_session_path)
     end
 
     it 'is redirected to user sign-in if it tries to open the user page' do
       visit user_index_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(new_user_session_path)
     end
 
     it 'is redirected to admin sign-in if it tries to open the admin page' do
       visit admin_index_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(new_admin_session_path)
     end
   end
@@ -60,42 +56,36 @@ RSpec.feature 'Authentication' do
     it 'can view the home page' do
       visit root_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(root_path)
     end
 
     it 'is redirected to the user index if it tries to view the user login page' do
       visit new_user_session_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(user_index_path)
     end
 
     it 'is redirected to the dashboard if it tries to view the user sign-up page' do
       visit new_user_registration_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(user_index_path)
     end
 
     it 'can view the user user page' do
       visit user_index_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(user_index_path)
     end
 
     it 'is redirected to the user page if it tries to view the admin login page' do
       visit new_admin_session_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(user_index_path)
     end
 
     it 'is redirected to the user page if it tries to view the admin dashboard page' do
       visit admin_index_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(user_index_path)
     end
   end
@@ -113,35 +103,30 @@ RSpec.feature 'Authentication' do
     it 'can view the home page' do
       visit root_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(root_path)
     end
 
     it 'is redirected to the admin page if it tries to view the user login page' do
       visit new_user_session_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(admin_index_path)
     end
 
     it 'is redirected to the admin page if it tries to view the user sign-up page' do
       visit new_user_registration_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(admin_index_path)
     end
 
     it 'is redirected to the admin page if it tries to view the user page' do
       visit user_index_path
 
-      expect(page.status_code).to eq(200)
       expect(page.current_path).to eq(admin_index_path)
     end
 
     it 'is redirected to the admin page if it tries to view the admin login page' do
       visit new_admin_session_path
-
-      expect(page.status_code).to eq(200)
+      
       expect(page.current_path).to eq(admin_index_path)
     end
   end
