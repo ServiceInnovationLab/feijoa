1ee4740a75f3bf50fe8d11832c3ebbf7ed07d107
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-11 14:03:37 +1200

    account type badge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  devise_group :account, contains: [:user, :admin_user]
+
   def after_sign_in_path_for(resource)
     return root_path if resource.blank?
 
