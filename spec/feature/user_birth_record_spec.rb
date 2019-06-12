a631aeae7e9bb34360e616c94e7b7c1a3061b465
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-13 10:44:55 +1200

    fix optional fields and test

--- spec/feature/user_birth_record_spec.rb
+++ spec/feature/user_birth_record_spec.rb
@@ -15,27 +15,53 @@ RSpec.describe 'user/BirthRecordsController', type: :feature do
         let(:target_record) { FactoryBot.create(:birth_record, family_name: 'Target-Person') }
         let(:birth_records) { FactoryBot.create_list(:birth_record, 10) }
 
-        before do
-          visit find_user_birth_records_path
-          fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
-          fill_in 'birth_record_family_name', with: target_record.family_name
-          fill_in 'birth_record_place_of_birth', with: target_record.place_of_birth
-          fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
+        context 'the required fields are filled in correctly' do
+          it 'is found' do
+            visit find_user_birth_records_path
+            fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
+            fill_in 'birth_record_family_name', with: target_record.family_name
+            fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
+
+            click_on 'Find'
+
+            expect(page).to have_selector('.card', count: 1)
+            expect(page).to have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
+          end
         end
 
-        it 'is found by an exact query' do
-          click_on 'Find'
+        context 'all the permitted fields are filled in correctly' do
+          it 'is found' do
+            visit find_user_birth_records_path
+            fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
+            fill_in 'birth_record_family_name', with: target_record.family_name
+            fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
 
-          expect(page).to have_selector('.card', count: 1)
-          expect(page).to have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
+            fill_in 'birth_record_place_of_birth', with: target_record.place_of_birth
+            fill_in 'birth_record_parent_first_and_middle_names', with: target_record.parent_first_and_middle_names
+            fill_in 'birth_record_parent_family_name', with: target_record.parent_family_name
+            fill_in 'birth_record_other_parent_first_and_middle_names', with: target_record.other_parent_first_and_middle_names
+            fill_in 'birth_record_other_parent_family_name', with: target_record.other_parent_family_name
+
+            click_on 'Find'
+
+            expect(page).to have_selector('.card', count: 1)
+            expect(page).to have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
+          end
         end
 
-        it 'no records are found for a non-matching query' do
-          fill_in 'birth_record_family_name', with: 'Not-Target-Person'
-          click_on 'Find'
+        context 'no fields are filled in' do
+          it 'no records are found' do
+            visit find_user_birth_records_path
+            fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
+            fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
 
-          expect(page).to_not have_css('.card')
-          expect(page).to_not have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
+            # This field doesn't match
+            fill_in 'birth_record_family_name', with: 'Not-Target-Person'
+
+            click_on 'Find'
+
+            expect(page).to_not have_css('.card')
+          end
         end
       end
     end
