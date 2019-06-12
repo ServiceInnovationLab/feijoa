6112c3fac2c78578862e26ad0d2580fa1d602a9d
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-12 14:22:26 +1200

    user birth record controller tests

---
+++ spec/feature/user_birth_record_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'user/BirthRecordsController', type: :feature do
+  describe '#query' do
+    context 'A User' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        login_as(user, :scope => :user)
+      end
+
+      context 'a target record and some other records exist' do
+        let(:target_record) { FactoryBot.create(:birth_record, family_name: 'Target-Person') }
+        let(:birth_records) { FactoryBot.create_list(:birth_record, 10) }
+
+        before do
+          visit find_user_birth_records_path
+          fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
+          fill_in 'birth_record_family_name', with: target_record.family_name
+          fill_in 'birth_record_place_of_birth', with: target_record.place_of_birth
+          fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
+        end
+
+        it 'is found by an exact query' do
+          click_on 'Find'
+
+          expect(page).to have_selector('.card', count: 1)
+          expect(page).to have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
+        end
+
+        it 'no records are found for a non-matching query' do
+          fill_in 'birth_record_family_name', with: 'Not-Target-Person'
+          click_on 'Find'
+
+          expect(page).to_not have_css('.card')
+          expect(page).to_not have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
+        end
+      end
+    end
+  end
+end
