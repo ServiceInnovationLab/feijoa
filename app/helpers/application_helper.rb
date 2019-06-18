9da60c02da33d90730353b6b8c37cce92ccd3e24
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-18 16:13:33 +1200

    improved and tested ApplicationHelper#action_in_bem_format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include ActiveSupport::Inflector
+
   def action_in_bem_format
-    # TODO: doesn't handle multiple directories
-    controller = request.params['controller'].gsub(%r{/}, '__')
-    action = request.params['action']
+    # split the full class name of the controller
+    tokens = params['controller']&.split('/')
+
+    path = dasherize(tokens[0..-2].join('-')).downcase
+    controller = dasherize(tokens.last).downcase
+    action = dasherize(params['action']).downcase
+
+    return "#{path}__#{controller}--#{action}" if path.present?
+
     "#{controller}--#{action}"
   end
 end
