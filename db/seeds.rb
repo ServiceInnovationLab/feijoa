a348700c243bf891d297c50fc59c12ef6cf7f1b9
codefactor-io <support@codefactor.io>
2019-06-18 02:15:10 +0000

    [CodeFactor] Apply fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,5 @@ FactoryBot.create_list :birth_record, 500
 
 ['brenda.wallace'].each do |person|
   email = "#{person}@dia.govt.nz"
-  unless AdminUser.find_by(email: email)
-    FactoryBot.create :admin_user, email: email
-  end
+  FactoryBot.create :admin_user, email: email unless AdminUser.find_by(email: email)
 end
