129bd502b62790c68c5eda44c2a002b5614f00d7
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-06-18 13:17:34 +1200

    Seed an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 FactoryBot.create_list :birth_record, 500
+
+['brenda.wallace'].each do |person|
+  email = "#{person}@dia.govt.nz"
+  unless AdminUser.find_by(email: email)
+    FactoryBot.create :admin_user, email: email
+  end
+end
