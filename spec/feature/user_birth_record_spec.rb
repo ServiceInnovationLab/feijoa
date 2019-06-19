b75b3feedb5134999b75021f5b5cbb14e0bd281e
Brenda <brenda@wallace.net.nz>
2019-06-19 12:51:58 +1200

    Update specs

--- spec/feature/user_birth_record_spec.rb
+++ spec/feature/user_birth_record_spec.rb
@@ -17,53 +17,54 @@ RSpec.describe 'user/BirthRecordsController', type: :feature do
 
         context 'the required fields are filled in correctly' do
           it 'is found' do
-            visit find_user_birth_records_path
-            fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
-            fill_in 'birth_record_family_name', with: target_record.family_name
-            fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
+            visit user_birth_records_path
+            click_link 'Search for Birth Record'
+            fill_in 'first_and_middle_names', with: target_record.first_and_middle_names
+            fill_in 'family_name', with: target_record.family_name
+            fill_in 'date_of_birth', with: target_record.date_of_birth
 
             click_on 'Find'
 
             expect(page).to have_selector('.card', count: 1)
-            expect(page).to have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
             Percy.snapshot(page, name: 'find birth record')
           end
         end
 
         context 'all the permitted fields are filled in correctly' do
           it 'is found' do
-            visit find_user_birth_records_path
-            fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
-            fill_in 'birth_record_family_name', with: target_record.family_name
-            fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
+            visit user_birth_records_path
+            click_link 'Search for Birth Record'
+            fill_in 'first_and_middle_names', with: target_record.first_and_middle_names
+            fill_in 'family_name', with: target_record.family_name
+            fill_in 'date_of_birth', with: target_record.date_of_birth
 
-            fill_in 'birth_record_place_of_birth', with: target_record.place_of_birth
+            fill_in 'place_of_birth', with: target_record.place_of_birth
             fill_in(
-              'birth_record_parent_first_and_middle_names',
+              'parent_first_and_middle_names',
               with: target_record.parent_first_and_middle_names
             )
-            fill_in 'birth_record_parent_family_name', with: target_record.parent_family_name
+            fill_in 'parent_family_name', with: target_record.parent_family_name
             fill_in(
-              'birth_record_other_parent_first_and_middle_names',
+              'other_parent_first_and_middle_names',
               with: target_record.other_parent_first_and_middle_names
             )
-            fill_in 'birth_record_other_parent_family_name', with: target_record.other_parent_family_name
+            fill_in 'other_parent_family_name', with: target_record.other_parent_family_name
 
             click_on 'Find'
 
             expect(page).to have_selector('.card', count: 1)
-            expect(page).to have_css("##{target_record.model_name.param_key}--#{target_record.primary_key_string}")
           end
         end
 
         context 'no fields are filled in' do
           it 'no records are found' do
-            visit find_user_birth_records_path
-            fill_in 'birth_record_first_and_middle_names', with: target_record.first_and_middle_names
-            fill_in 'birth_record_date_of_birth', with: target_record.date_of_birth
+            visit user_birth_records_path
+            click_link 'Search for Birth Record'
+            fill_in 'first_and_middle_names', with: target_record.first_and_middle_names
+            fill_in 'date_of_birth', with: target_record.date_of_birth
 
             # This field doesn't match
-            fill_in 'birth_record_family_name', with: 'Not-Target-Person'
+            fill_in 'family_name', with: 'Not-Target-Person'
 
             click_on 'Find'
 
