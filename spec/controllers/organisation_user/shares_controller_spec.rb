a03784efe618d2c9679cd746157ccb458d787718
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-21 15:45:06 +1200

    test that unauthorized users are redirected

--- spec/controllers/organisation_user/shares_controller_spec.rb
+++ spec/controllers/organisation_user/shares_controller_spec.rb
@@ -28,4 +28,79 @@ RSpec.describe OrganisationUser::SharesController, type: :controller do
       end
     end
   end
+
+  context 'a User' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:organisation_user) { FactoryBot.create(:organisation_user) }
+    let(:share) { FactoryBot.create(:share, user: user, recipient: organisation_user) }
+
+    before do
+      sign_in user
+      share
+    end
+
+    describe 'GET #index' do
+      it 'is redirected away' do
+        get :index
+        expect(response).to redirect_to(new_organisation_user_session_path)
+      end
+    end
+
+    describe 'GET #show' do
+      it 'is redirected away' do
+        get :show, params: { id: share.to_param }
+        expect(response).to redirect_to(new_organisation_user_session_path)
+      end
+    end
+  end
+
+  context 'an Admin user' do
+    let(:admin_user) { FactoryBot.create(:admin_user) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:organisation_user) { FactoryBot.create(:organisation_user) }
+    let(:share) { FactoryBot.create(:share, user: user, recipient: organisation_user) }
+
+    before do
+      sign_in admin_user
+      share
+    end
+
+    describe 'GET #index' do
+      it 'is redirected away' do
+        get :index
+        expect(response).to redirect_to(new_organisation_user_session_path)
+      end
+    end
+
+    describe 'GET #show' do
+      it 'is redirected away' do
+        get :show, params: { id: share.to_param }
+        expect(response).to redirect_to(new_organisation_user_session_path)
+      end
+    end
+  end
+
+  context 'an unauthenticated guest user' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:organisation_user) { FactoryBot.create(:organisation_user) }
+    let(:share) { FactoryBot.create(:share, user: user, recipient: organisation_user) }
+
+    before do
+      share
+    end
+
+    describe 'GET #index' do
+      it 'is redirected away' do
+        get :index
+        expect(response).to redirect_to(new_organisation_user_session_path)
+      end
+    end
+
+    describe 'GET #show' do
+      it 'is redirected away' do
+        get :show, params: { id: share.to_param }
+        expect(response).to redirect_to(new_organisation_user_session_path)
+      end
+    end
+  end
 end
