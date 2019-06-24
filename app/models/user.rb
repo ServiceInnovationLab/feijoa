943152540f8ef098b55b40f71bad412c283c4f31
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-24 13:08:00 +1200

    refactor user.audits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  audited
-  has_associated_audits
-
   # Include default devise modules. Others available are:
   # :lockable, :confirmable, :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,4 +10,16 @@ class User < ApplicationRecord
   has_many :birth_records_users, dependent: :destroy
   has_many :birth_records, -> { distinct }, through: :birth_records_users
   has_many :shares, dependent: :nullify
+
+  # Get the audits for this user 
+  #
+  # BirthRecordsUser (the join table), Share, and View (TODO not implemented
+  # yet, requires the 'view shared birth records' feature) audits are all
+  # associated with a BirthRecord so the easiest way to gat a user's audits is
+  # to get the associated audits for their birth records.
+  def audits
+    birth_records
+      .map(&:own_and_associated_audits)
+      .flatten
+  end
 end
