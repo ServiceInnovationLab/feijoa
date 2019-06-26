addefa7853be4706d1c0708871c031805b47ee82
Lyall Morrison <lyall@rabidtech.co.nz>
2019-06-27 11:21:37 +1200

    rubocooooooop!

--- spec/views/admin/application/_flashes.html.erb_spec.rb
+++ spec/views/admin/application/_flashes.html.erb_spec.rb
@@ -1,49 +1,50 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe "rendering flash messages in admin/application/flashes" do
-  it "renders a raw html string of the alert and notice values" do
+require 'rails_helper'
+
+RSpec.describe 'rendering flash messages in admin/application/flashes' do
+  it 'renders a raw html string of the alert and notice values' do
     flash = {
-      :alert=>"Your session expired. Please sign in again to continue.",
-      :notice=>"A notice",
-      :system=>"this is not rendered"
+      alert: 'Your session expired. Please sign in again to continue.',
+      notice: 'A notice',
+      system: 'this is not rendered'
     }
 
-    render "admin/application/flashes", :flash => flash
+    render 'admin/application/flashes', flash: flash
 
-    expect(rendered).to include("Your session expired. Please sign in again to continue.")
-    expect(rendered).to include("A notice")
-    expect(rendered).not_to include("this is not rendered")
-
+    expect(rendered).to include('Your session expired. Please sign in again to continue.')
+    expect(rendered).to include('A notice')
+    expect(rendered).not_to include('this is not rendered')
   end
 
-  it "renders non-string values with their string representation" do
+  it 'renders non-string values with their string representation' do
     flash = {
-      :alert=>true
+      alert: true
     }
 
-    render "admin/application/flashes", :flash => flash
+    render 'admin/application/flashes', flash: flash
 
     expect(rendered).to include(true.to_s)
   end
 
-  it "escapes html in values" do
+  it 'escapes html in values' do
     flash = {
-      :alert=>"<>"
+      alert: '<>'
     }
 
-    render "admin/application/flashes", :flash => flash
+    render 'admin/application/flashes', flash: flash
 
-    expect(rendered).to include("&lt;&gt;")
+    expect(rendered).to include('&lt;&gt;')
   end
 
-  it "strips scripts from values" do
+  it 'strips scripts from values' do
     flash = {
-      :alert=>"Some text and <script>malicious javascript</script>"
+      alert: 'Some text and <script>malicious javascript</script>'
     }
 
-    render "admin/application/flashes", :flash => flash
+    render 'admin/application/flashes', flash: flash
 
-    expect(rendered).to include("Some text and malicious javascript")
-    expect(rendered).not_to include("script>")
+    expect(rendered).to include('Some text and malicious javascript')
+    expect(rendered).not_to include('script>')
   end
 end
