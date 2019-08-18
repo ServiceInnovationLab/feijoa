7eb0a0d51d64c2095f13479bfed2b5fe3edd438b
Jacob <jacob.sien.ong@gmail.com>
2019-08-19 10:34:35 +1200

    Feature spec for organisations table.

---
+++ spec/features/organisations_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'organisations page', type: :feature do
+  before do
+    visit '/organisations'
+  end
+
+  it 'displays table heading' do
+    expect(page).to have_text('Organisations')
+  end
+
+  it 'displays a table of organisations' do
+    expect(page).to have_text('Name')
+    expect(page).to have_text('Address')
+    expect(page).to have_text('Email')
+    expect(page).to have_text('Contact No.')
+    expect(page).to have_text('List of organisations')
+  end
+end
