85985b9143daaa66610fb34b318efe9d4737d814
Jacob <jacob.sien.ong@gmail.com>
2019-08-19 13:48:08 +1200

    User spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Factory' do
-    subject { FactoryBot.create(:user) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:organisation) { FactoryBot.create(:organisation) }
 
-    it 'is valid' do
-      expect(subject).to be_valid
+    context 'user has the role' do
+      before do
+        OrganisationMember.create!(organisation: organisation, user: user, role: 'boss')
+      end
+
+      it 'states user role' do
+        expect(user.role_for(organisation)).to eq('boss')
+      end
     end
+
+    context 'user has no roles' do
+      it 'states user role' do
+        expect(user.role_for(organisation)).to eq nil
+      end
+    end
+    # user.admin_for?(organisation)
+    # Should return false when a user isn't a member of the organisation
+    # Should return false when a user has a role other than admin with the organisation
+    # Should return true when a user is a member of the organisation and has the admin role
+
+    # user.member_of?(organisation)
+    # Should return false when a user isn't a member of the organisation
+    # Should return true when a user is a member of the organisation
   end
 end
