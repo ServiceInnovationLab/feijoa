3e52b92097ab7be2f2c568d30beaa6c650dc70f5
Jacob <jacob.sien.ong@gmail.com>
2019-08-19 13:11:18 +1200

    Added more Faker fields & extended the organisation spec.

--- spec/features/organisations_spec.rb
+++ spec/features/organisations_spec.rb
@@ -3,20 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'organisations page', type: :feature do
+  let!(:orgs) { FactoryBot.create_list :organisation, 5 }
+
   before do
     visit '/organisations'
   end
 
-  it 'displays table heading' do
-    expect(page).to have_text('Organisations')
+  describe 'displays table heading' do
+    it { expect(page).to have_text('Organisations') }
   end
 
-  it 'displays a table of organisations' do
-    expect(page).to have_css('.table.table-hover')
-    expect(page).to have_text('Name')
-    expect(page).to have_text('Address')
-    expect(page).to have_text('Email')
-    expect(page).to have_text('Contact No.')
-    expect(page).to have_text('List of organisations')
+  describe 'displays a table of organisations' do
+    it { expect(page).to have_css('.table.table-hover') }
+    it { expect(page).to have_text('Name') }
+    it { expect(page).to have_text('Address') }
+    it { expect(page).to have_text('Email') }
+    it { expect(page).to have_text('Contact No.') }
+    it { expect(page).to have_text('List of organisations') }
+  end
+
+  describe 'displays an organisation' do
+    it { expect(page).to have_content(orgs.first.name) }
+    it { expect(page).to have_content(orgs.last.name) }
+    it { expect(page).to have_content(orgs.first.address) }
+    it { expect(page).to have_content(orgs.last.address) }
+    it { expect(page).to have_content(orgs.first.email) }
+    it { expect(page).to have_content(orgs.last.email) }
+    it { expect(page).to have_content(orgs.first.contact_number) }
+    it { expect(page).to have_content(orgs.last.contact_number) }
   end
 end
