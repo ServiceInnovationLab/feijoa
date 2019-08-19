d4fd3a9e387d769c276ef7729e3e86fdfaa1833a
Merrin <merrin.macleod@gmail.com>
2019-08-19 14:38:43 +1200

    fix remaining tests

--- spec/features/audit_spec.rb
+++ spec/features/audit_spec.rb
@@ -39,6 +39,7 @@ RSpec.feature 'Auditing' do
 
         context 'they remove the birth record' do
           before do
+            click_on 'View'
             page.accept_alert 'Are you sure? This will remove the record from your documents' do
               click_on 'Remove'
             end
