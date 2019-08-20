c896af652c99018593a5927067be298d5f90456d
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-08-21 11:13:41 +1200

    Rails validates organisaion record data source uniquness

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -7,6 +7,7 @@ class Organisation < ApplicationRecord
   has_many :requests, inverse_of: 'requester', foreign_key: 'requester_id', dependent: :destroy
 
   validates :name, presence: true
+  validates :data_source_key, uniqueness: { scope: :data_source_name }, allow_nil: true
 
   def add_admin(user)
     organisation_members.create(user: user, role: OrganisationMember::ADMIN_ROLE)
--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Organisation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'when a record already exist with same data source key and name' do
+    let(:ds_name) { 'chocolate' }
+    let(:ds_id) { '123' }
+    before { FactoryBot.create :organisation, data_source_key: ds_id, data_source_name: ds_name }
+
+    it "doesn't allow duplicate records for same data_source record" do
+      expect do
+        FactoryBot.create :organisation, data_source_key: ds_id, data_source_name: ds_name
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+    it do
+      expect(FactoryBot.build(:organisation, data_source_key: ds_id, data_source_name: ds_name).valid?).to eq false
+    end
+  end
+  describe 'allows nil data_source on organisations' do
+    before { FactoryBot.create :organisation, data_source_key: nil, data_source_name: nil }
+    it do
+      expect do
+        FactoryBot.create :organisation, data_source_key: nil, data_source_name: nil
+      end.not_to raise_error
+    end
+  end
 end
