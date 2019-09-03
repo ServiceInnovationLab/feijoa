a101398b967d6cae0ab528a9902f5130fb6816c8
Merrin <merrin.macleod@gmail.com>
2019-09-03 15:06:17 +1200

    remove thing I didnt end up using

--- app/helpers/document_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module DocumentHelper
-  def partial_for_document(document)
-    underscore_class_name = document.class.to_s.underscore
-    ["shared/documents/full/#{underscore_class_name}", locals: { underscore_class_name => @document }]
-  end
-end
