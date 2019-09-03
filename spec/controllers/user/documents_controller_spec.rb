375adb0b5043039063d8ad87d6f521ad22e79775
Merrin <merrin.macleod@gmail.com>
2019-09-04 11:29:46 +1200

    add test for documents#remove

--- spec/controllers/user/documents_controller_spec.rb
+++ spec/controllers/user/documents_controller_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe User::DocumentsController do
       end
     end
   end
+
+  describe 'remove' do
+    include_context 'document types' do
+      it 'removes any document type' do
+        one_of_each_document_type.each do |document|
+          document.add_to(user)
+          post(:remove, params: { id: document.id, type: document.document_type })
+          expect(user.documents).not_to include(document)
+          expect(response).to have_http_status(302) #redirected
+        end
+      end
+    end
+  end
 end
