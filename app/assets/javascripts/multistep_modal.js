eeaa1239d2ab7772d3659c0c663d69d5df31aeb4
Merrin <merrin.macleod@gmail.com>
2019-09-19 12:52:44 +1200

    remove more unused things

--- app/assets/javascripts/multistep_modal.js
+++ app/assets/javascripts/multistep_modal.js
@@ -4,10 +4,7 @@
 var initializeModals = function(selector){
   var modals = $(selector);
   modals.each(function(idx, modal) {
-    console.log(modal)
     var $modal = $(modal);
-    var $bodies = $modal.find('div.modal-body');
-    var reset_on_close = $modal.attr('reset-on-close') === 'true';
 
     function reset() {
       $modal.find('.step').hide();
@@ -17,12 +14,7 @@ var initializeModals = function(selector){
     function goToStep(step) {
       reset();
       var to_show = $modal.find('.step-' + step);
-      if (to_show.length === 0) {
-        // at the last step, nothing else to show
-        return;
-      }
-      to_show.show();
-      var current = parseInt(step, 10);
+      if (to_show.length !== 0) { to_show.show(); }
     }
 
     $modal.find('[data-trigger]').on('click', function(){
@@ -32,12 +24,10 @@ var initializeModals = function(selector){
     function initialize() {
       reset();
       $modal.find('.step-1').show();
-      if (reset_on_close){
-        $modal.on('hidden.bs.modal', function () {
-          reset();
-          $modal.find('.step-1').show();
-        })
-      }
+      $modal.on('hidden.bs.modal', function () {
+        reset();
+        $modal.find('.step-1').show();
+      })
     }
 
     initialize();
