ec2b914e140ae52bb1233f1e5768b738b74ea55a
Merrin Macleod <merrin.macleod@gmail.com>
2019-09-19 14:03:47 +1200

    🏯Make a multistep modal for document adding (#207)
    * make a multistep modal for document adding
    
    * fix tests and rubocop
    
    * reset on close and remove unused things from multi step modal js
    
    * remove more unused things
    
    * declare function old way
    
    * declare and use in same file to satisfy codeclimate

---
+++ app/assets/javascripts/multistep_modal.js
@@ -0,0 +1,39 @@
+// adapted from https://github.com/ngzhian/multi-step-modal
+'use strict';
+
+function initializeModals(selector){
+  var modals = $(selector);
+  modals.each(function(idx, modal) {
+    var $modal = $(modal);
+
+    function reset() {
+      $modal.find('.step').hide();
+      $modal.find('[data-step]').hide();
+    }
+
+    function goToStep(step) {
+      reset();
+      var to_show = $modal.find('.step-' + step);
+      if (to_show.length !== 0) { to_show.show(); }
+    }
+
+    $modal.find('[data-trigger]').on('click', function(){
+      goToStep($(this).data('trigger'));
+    })
+
+    function initialize() {
+      reset();
+      $modal.find('.step-1').show();
+      $modal.on('hidden.bs.modal', function () {
+        reset();
+        $modal.find('.step-1').show();
+      })
+    }
+
+    initialize();
+  })
+}
+
+$(document).ready(function(){
+  initializeModals('.modal.js-multi-step');
+})
